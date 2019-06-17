1ce8b544df6ccfea317db1112a4cb1b4a2dd8d65
GABRIELE Giunchi <gabriele.giunchi-cic-it@ibm.com>
2019-06-17 13:51:05 +0200

    users and gyms have their own table

--- src/main/kotlin/com/gabrigiunchi/backendtesi/model/entities/Drawable.kt
+++ src/main/kotlin/com/gabrigiunchi/backendtesi/model/entities/Drawable.kt
@@ -1,11 +1,9 @@
 package com.gabrigiunchi.backendtesi.model.entities
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 open class Drawable(
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
